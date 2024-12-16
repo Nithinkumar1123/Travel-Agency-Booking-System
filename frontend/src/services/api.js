f4bada69b609b738f44afc7152dcc1f713741a64
NITHIN KUMAR S <156948837+Nithinkumar1123@users.noreply.github.com>
2024-12-16 18:00:23 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // Make sure this matches your backend's URL
+  baseURL: 'https://travel-agency-booking-system-rps0.onrender.com', // Make sure this matches your backend's URL
 });
 
 export default api;
